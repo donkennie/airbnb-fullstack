10aaa5d126d1a03ba333696173794b42cfb2db7c
donkennie <ajeigbekehinde160@gmail.com>
2024-10-17 20:31:39 +0100

    install prisma package

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,9 +1,9 @@
 'use client'
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 const Logo = () => {
-    const router = useRouter();
+    // const router = useRouter();
 
     return(
         <Image
